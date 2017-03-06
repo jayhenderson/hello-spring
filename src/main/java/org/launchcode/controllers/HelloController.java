02624df9eaa5d8a9fe9fe34b071dc08abb9c0c10
Jay Henderson <jjhenderson@gmail.com>
2017-03-06 16:36:45 -0600

    Add controllers from video lessons

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,9 +1,9 @@
 package org.launchcode.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.xml.ws.RequestWrapper;
 
 /**
@@ -14,15 +14,51 @@ public class HelloController {
 
     @RequestMapping(value = "")
     @ResponseBody
-    public String index() {
-        return "Hello World";
+    public String index(HttpServletRequest request) {
+
+        String username = request.getParameter("name");
+
+        if (username == null) {
+            username = "World";
+        }
+
+        return "Hello " + username;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm() {
+
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
+        return html;
+    }
+    @RequestMapping(value = "hello", method= RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request) {
+
+
+        String name = request.getParameter("name");
+
+        return "Hello " + name;
+
+    }
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
+
+        return "Hello " + name;
+
     }
 
 
     @RequestMapping(value = "goodbye")
-    @ResponseBody
     public String goodbye() {
-        return "Goodbye";
+        return "redirect:/";
     }
 
 }
